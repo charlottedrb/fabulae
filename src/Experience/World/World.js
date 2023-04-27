5263ab4f6a3a4fb7010020fe12e2eeb0f1566013
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2023-04-27 14:29:49 +0200

    🐣 [INIT] Init project

---
+++ src/Experience/World/World.js
@@ -0,0 +1,26 @@
+import Experience from '../Experience.js'
+import Environment from './Environment.js'
+import Floor from './Floor.js'
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        // Wait for resources
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            this.floor = new Floor()
+            this.environment = new Environment()
+        })
+    }
+
+    update()
+    {
+
+    }
+}
