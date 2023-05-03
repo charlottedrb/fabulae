cc37b54d25cbf0a4a6a2a047a6b13844f4bc6ba5
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2023-05-03 23:47:22 +0200

    center titles and add button

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,28 @@ export default {
       'dubai': ['Dubai', 'sans-serif'],
       'roystorie': ['Roystorie', 'serif'],
     },
-    padding: {
+    spacing: {
+      300: '30rem',
+      150: '15rem',
+      50: '5rem',
+      35: '3.5rem',
+      25: '2.5rem',
       0: 0,
       container: '5rem'
     },
-    spacing: {
-      50: '5rem',
-      0: 0,
-    },
-    padding: {
-      300: '30rem',
-      150: '15rem',
-      0: 0,
-
-    },
     fontSize: {
-      200: '200px'
+      150: '15rem',
+      22: '2.2rem',
+      18: '1.8rem',
+    },
+    lineHeight: {
+      '1.6' : '160%'
+    },
+    borderRadius: {
+      10: '1rem',
+    },
+    borderWidth: {
+      px: '1px',
     },
     extend: {
       width: {
