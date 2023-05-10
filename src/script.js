aeec0ef42833b8fa8a2ba92bc04d99995cc63d80
Charlotte Der Baghdassarian <c.derbaghdassarian@gmail.com>
2023-05-10 14:24:45 +0200

    init data manager

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 
+import DataManager from './Experience/Data/DataManager.js'
 import Experience from './Experience/Experience.js'
 import * as Taxi from '@unseenco/taxi'
 
 const taxi = new Taxi.Core()
 
+const dataManager = new DataManager()
 const experience = new Experience(document.querySelector('canvas.webgl'))
