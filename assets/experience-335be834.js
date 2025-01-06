ef34627f08a330c5ac9b4e3f50dc06c22b478be6
charlottedrb <charlottedrb@users.noreply.github.com>
2025-01-06 18:49:07 +0000

    deploy: d1c7c926dbf65cb1eaa056e981484c72f20b8728

--- assets/experience-335be834.js
+++
@@ -1,2 +0,0 @@
-import{C as e,E as r}from"./Experience-1b8d4a2c.js";new e;new r(document.querySelector("canvas.webgl"));
-//# sourceMappingURL=experience-335be834.js.map
---
+++ assets/experience-5238c3ca.js
@@ -0,0 +1,2 @@
+import{C as e,E as r}from"./Experience-a9ebc1ef.js";new e;new r(document.querySelector("canvas.webgl"));
+//# sourceMappingURL=experience-5238c3ca.js.map
